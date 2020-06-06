22acfb45782ea587cff417d6832afd55fbea72bd
2293736867 <2293736867@qq.com>
2020-06-06 16:49:33 +0800

    reset

--- project/Backend/src/main/java/com/test/repository/WorkerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.test.repository;
-import com.test.entity.Worker;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkerRepository extends CrudRepository<Worker,String> {
-    boolean existsByCellphoneAndPassword(String cellphone,String password);
-}
